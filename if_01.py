c6d9a23013b92f30ede7ee9747c2516ab2ce47a4
Your Name <you@example.com>
2021-05-25 15:31:19 +0900

    함수 step1

---
+++ if_01.py
@@ -0,0 +1,15 @@
+# 나이 = 19
+# if 나이 < 20:
+#     print("청소년 할인")
+# walk = 1999
+# if walk >= 1000:
+#     print("목표 달성")
+    
+
+now = 11
+
+if now < 12:
+    print("오전입니다")
+else:
+    print("오후입니다")
+    
---
+++ rsp_input.py
@@ -0,0 +1,20 @@
+# selected = None
+# while selected not in ['가위', '바위', '보']:
+#   selected = input('가위, 바위, 보 중에서 선택하세요> ')
+# print('선택한 값은:', selected)
+
+
+
+selected = None
+com_selected = '가위'
+while selected not in ['가위', '바위', '보']:
+    selected = input('가위, 바위, 보 중에서 선택하세요>')
+    if (selected == '가위' or selected == '바위' or selected == '보'):
+        if(selected == '바위'):
+            selected == selected
+            print('You Win')
+        else:
+            selected = None
+            print('이길때 까지 못나와')
+    else:
+        print('가위 바위 보 중에 하나 선택하세요')
